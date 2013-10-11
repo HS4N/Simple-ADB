0991fb0bdb0a292f88c0677f77827f515b585c71
HS4N <hs4n@live.com>
2013-10-11 12:09:12 -0700

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿/* Made by HS4N
+*  Follow me on twitter http://twitter.com/HS4N
+*  Thanks! */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading; 
+
+namespace SplashScreen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Thread t = new Thread(new ThreadStart(SplashScreen));
+            t.Start();  
+            Thread.Sleep(5000); 
+            InitializeComponent();
+            t.Abort(); 
+            
+        }
+
+        public void SplashScreen()
+        {
+            Application.Run(new Form2());  
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,28 @@
+﻿/* Made by HS4N 
+*  Follow me on twitter http://twitter.com/HS4N
+*  Thanks! */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SplashScreen
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Increment(1);
+            if (progressBar1.Value == 100) timer1.Stop(); 
+        }
+    }
+}
